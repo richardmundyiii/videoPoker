232fb7dec664333edf95917fb001be4874c35ab3
richardmundyiii <mundy.richard@icloud.com>
2023-01-19 16:58:22 -0800

    project planning

---
+++ projectPlanning/sudo.js
@@ -0,0 +1,13 @@
+/* 
+
+1)  Board loads with 5 cards upside down. Deal button is diabled. Hold signs hidden.
+2) User needs to select between 1 & 5 "Credits"
+    As bet credits increase, the corrosponding column in the payout structure table will slightly highlight to indicate what players are potentially playing for.
+    IF, max bet has beeen selected, "bet one" and "max bet" buttons disbale. 
+3) When player is ready, press 'DEAL' button will shuffle deck and displays the first 5 cards.
+    At this point, the bet buttons will be disabled.
+4) Player can then choose to hold 0 through 5 cards. If they select a card to hold, the hold visual will appear above the card.
+5) Player will then press 'DEAL' button again to have the next cards in the array display in place of the cards not held. Filling the missing cards from left to right.
+6) If the player has NOT won the game will end. 'DEAL' button will become diabled again.
+    If player wins, the winning hand will illuminate in the payout structer and the credit will be added to the total credits on the bottom right of the screen. The amount won on that hand will be displayed in lower left of screen.
+*/
